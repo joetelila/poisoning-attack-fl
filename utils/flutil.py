1976bcba0d6640d688c26cd1b591b8d4c75adbb2
Joe Telila <joetelila@Joes-Mac-mini.local>
2024-03-02 11:55:07 -0500

    Implemented basic FL and attack

---
+++ utils/flutil.py
@@ -0,0 +1,9 @@
+class Flutils:
+    def __init__(self, name):
+        self.name = name
+
+    def attack(self):
+        print(f"Hello, {self.name}!")
+
+    def defend(self, num1, num2):
+        return num1 + num2
